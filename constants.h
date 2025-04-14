e5558bd06e28666005b91875324ca922d27ec991
catilus <helene.m.piet@gmail.com>
2025-04-13 18:58:47 -0700

    Added a few basic files

---
+++ constants.h
@@ -0,0 +1,19 @@
+/* 
+constants.h
+-----------
+
+By Catilus
+
+Created: 2025-04-13
+Last modified: 2025-04-13
+
+Role: defines constants for the entire game
+
+*/
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,26 @@
+/* 
+main.c
+-----------
+
+By Catilus
+
+Created: 2025-04-13
+Last modified: 2025-04-13
+
+Role: main
+
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "constants.h"
+
+
+int main(int argc, char *argv[])
+{
+
+
+}
